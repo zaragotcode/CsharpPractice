f21ce9124f3c63b5c901dc8969453929596cd708
Patrick Mazara <patrickmazara@hotmail.com>
2023-10-09 19:26:18 -0400

    Complete code challenge: Implement a method of the Math class that returns the larger of two numbers

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
